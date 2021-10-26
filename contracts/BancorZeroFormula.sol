5999290b1e9f19e89528f995bb9d2e2e05a9c67e
cbobrobison <cbobrobison@gmail.com>
2021-10-26 14:51:51 -0700

    renamed reserveWeight to connectorWeight

--- contracts/BancorZeroFormula.sol
+++ contracts/BancorZeroFormula.sol
@@ -14,34 +14,34 @@ contract BancorZeroForumula {
 
     /// @notice Given a deposit (in the connector token), reserve weight, Token supply and
     ///     balance pooled, calculate the return for a given conversion (in the Token)
-    /// @dev _supply * ((1 + _tokensDeposited / _balancePooled) ^ (_reserveWeight / 1000000) - 1)
+    /// @dev _supply * ((1 + _tokensDeposited / _balancePooled) ^ (_connectorWeight / 1000000) - 1)
     /// @param _tokensDeposited   amount of collateral tokens to deposit
-    /// @param _reserveWeight   connector weight, represented in ppm, 1 - 1,000,000
+    /// @param _connectorWeight   connector weight, represented in ppm, 1 - 1,000,000
     /// @param _supply          current Token supply
     /// @param _balancePooled   total connector balance
     /// @return amount of Tokens minted
     function _calculateMintReturn(
         uint256 _tokensDeposited,
-        uint32 _reserveWeight,
+        uint32 _connectorWeight,
         uint256 _supply,
         uint256 _balancePooled
     ) private view returns (uint256) {
         // validate input
         require(
             _balancePooled > 0 &&
-                _reserveWeight > 0 &&
-                _reserveWeight <= maxWeight
+                _connectorWeight > 0 &&
+                _connectorWeight <= maxWeight
         );
         // special case for 0 deposit amount
         if (_tokensDeposited == 0) {
             return 0;
         }
         // special case if the weight = 100%
-        if (_reserveWeight == maxWeight) {
+        if (_connectorWeight == maxWeight) {
             return (_supply * _tokensDeposited) / _balancePooled;
         }
 
-        bytes16 exponent = uint256(_reserveWeight).fromUInt().div(
+        bytes16 exponent = uint256(_connectorWeight).fromUInt().div(
             uint256(maxWeight).fromUInt()
         );
         bytes16 part1 = _one.add(
@@ -56,7 +56,7 @@ contract BancorZeroForumula {
 
     /// @notice Given a deposit (in the collateral token) Token supply of 0, constant x and
     ///         constant y, calculates the return for a given conversion (in the Token)
-    /// @dev  [tokensDeposited / (reserveWeight * baseX * baseY) / baseX ^ (MAX_WEIGHT/reserveWeight)] ^ reserveWeight
+    /// @dev  [tokensDeposited / (connectorWeight * baseX * baseY) / baseX ^ (MAX_WEIGHT/connectorWeight)] ^ connectorWeight
     /// @dev  _baseX and _baseY are needed as Bancor formula breaks from a divide-by-0 when supply=0
     /// @param _tokensDeposited     amount of collateral tokens to deposit
     /// @param _baseX               constant x (arbitrary point in supply)
@@ -64,37 +64,37 @@ contract BancorZeroForumula {
     /// @return amount of Tokens minted
     function _calculateMintReturnFromZero(
         uint256 _tokensDeposited,
-        uint256 _reserveWeight,
+        uint256 _connectorWeight,
         uint256 _baseX,
         uint256 _baseY
     ) private view returns (uint256) {
-        // (MAX_WEIGHT/reserveWeight)
+        // (MAX_WEIGHT/connectorWeight)
         bytes16 exponent = uint256(maxWeight)
             .fromUInt()
-            .div(_reserveWeight.fromUInt());
-        // baseX ^ (MAX_WEIGHT/reserveWeight)
+            .div(_connectorWeight.fromUInt());
+        // baseX ^ (MAX_WEIGHT/connectorWeight)
         bytes16 denominator_denominator = (_baseX.fromUInt().ln().mul(exponent).exp());
         // Instead of calculating "x ^ exp", we calculate "e ^ (log(x) * exp)"
-        // reserveWeight * baseX * baseY
-        bytes16 denominator = _reserveWeight.mul(_baseX).mul(_baseY);
-        // tokensDeposited / (reserveWeight * baseX * baseY) / baseX ^ (MAX_WEIGHT/reserveWeight)
+        // connectorWeight * baseX * baseY
+        bytes16 denominator = _connectorWeight.mul(_baseX).mul(_baseY);
+        // tokensDeposited / (connectorWeight * baseX * baseY) / baseX ^ (MAX_WEIGHT/connectorWeight)
         bytes16 base = _tokensDeposited.div(denominator).div(denominator_denominator);
-        // [tokensDeposited / (reserveWeight * baseX * baseY) / baseX ^ (MAX_WEIGHT/reserveWeight)] ^ reserveWeight
-        bytes16 res = (base.fromUInt().ln().mul(_reserveWeight).exp());
+        // [tokensDeposited / (connectorWeight * baseX * baseY) / baseX ^ (MAX_WEIGHT/connectorWeight)] ^ connectorWeight
+        bytes16 res = (base.fromUInt().ln().mul(_connectorWeight).exp());
         return res.toUInt();
     }
 
     /// @notice Given an amount of Tokens to burn, connector weight, supply and collateral pooled,
     ///     calculates the return for a given conversion (in the collateral token)
-    /// @dev _balancePooled * (1 - (1 - _TokensBurned/_supply) ^ (1 / (_reserveWeight / 1000000)))
+    /// @dev _balancePooled * (1 - (1 - _TokensBurned/_supply) ^ (1 / (_connectorWeight / 1000000)))
     /// @param _TokensBurned        amount of Tokens to burn
-    /// @param _reserveWeight       connector weight, represented in ppm, 1 - 1,000,000
+    /// @param _connectorWeight       connector weight, represented in ppm, 1 - 1,000,000
     /// @param _supply              current Token supply
     /// @param _balancePooled       total connector balance
     /// @return amount of collateral tokens received
     function _calculateBurnReturn(
         uint256 _TokensBurned,
-        uint32 _reserveWeight,
+        uint32 _connectorWeight,
         uint256 _supply,
         uint256 _balancePooled
     ) private view returns (uint256) {
@@ -102,8 +102,8 @@ contract BancorZeroForumula {
         require(
             _supply > 0 &&
                 _balancePooled > 0 &&
-                _reserveWeight > 0 &&
-                _reserveWeight <= maxWeight &&
+                _connectorWeight > 0 &&
+                _connectorWeight <= maxWeight &&
                 _TokensBurned <= _supply
         );
         // special case for 0 sell amount
@@ -115,12 +115,12 @@ contract BancorZeroForumula {
             return _balancePooled;
         }
         // special case if the weight = 100%
-        if (_reserveWeight == maxWeight) {
+        if (_connectorWeight == maxWeight) {
             return (_balancePooled * _TokensBurned) / _supply;
         }
-        // 1 / (reserveWeight/MAX_WEIGHT)
+        // 1 / (connectorWeight/MAX_WEIGHT)
         bytes16 exponent = _one.div(
-            uint256(_reserveWeight).fromUInt().div(
+            uint256(_connectorWeight).fromUInt().div(
                 uint256(maxWeight).fromUInt()
             )
         );
